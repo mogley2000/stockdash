00d0831c88e281a8ff9d73a538936e474696a83c
Monkey <yang.leo@gmail.com>
2022-03-06 07:42:00 +1100

    graphs work

--- app.py
+++ app.py
@@ -1,213 +1,226 @@
-""" app.py is being run by Flask as a webserver and looking to app.py. 
-Can't run usual Python code here i.e. print("Hello World") 
-"""
+# Code source: https://dash-bootstrap-components.opensource.faculty.ai/examples/simple-sidebar/
 
-from flask import Flask, render_template, request, jsonify, send_file # Flask is a Class inside flask library 
+import dash
+import dash_bootstrap_components as dbc
+from dash import html
+from dash import dcc
+import plotly.express as px
+from dash.dependencies import Input, Output
+import pandas as pd
 import yfinance as yf
-from matplotlib import pyplot as plt
-from matplotlib.figure import Figure # Import Figure Class 
-from plot import get_closing_prices, plot_graph, small_plot # Must important functions from another module. 
-                                                # App module is for routing only 
-from io import BytesIO
 
-#yFinance 
-def get_closing_prices(symbol, period, interval):
-    """ Pull data from yf"""
-    quote = yf.Ticker(symbol)
-    hist = quote.history(period, interval)
-    return hist["Close"].round(decimals=2)
+import page_1
+
+external_stylesheets = [dbc.themes.CYBORG] #DARKLY
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
 
-def performance(stock_ticker, stock_period, stock_interval): 
-    """Generate percentage return over stock_period
-    """
-    # Call yfinance
-    data = get_closing_prices(stock_ticker, stock_period, stock_interval) 
+# styling the sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    # "background-color": "#4C4C4C",
+}
+
+# padding for the page content
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+    "background-color": "#060606"
+}
+
+SIDEBAR = html.Div(
+    [
+        html.H2("Sidebard / Stocks", className="display-4"),
+        html.Hr(),
+        html.P(
+            "Number of students per education level", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Test", href="/test", active="exact"),
+                dbc.NavLink("NVDA", href="/NVDA", active="exact"),
+                dbc.NavLink("MSFT", href="/MSFT", active="exact"),
+                dbc.NavLink("AAPL", href="/AAPL", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+
+CONTENT = html.Div(id="page-content", children=[], style=CONTENT_STYLE)  # Graph goes inside children via call back 
+
+CALLBACK = html.Div([   
+                dcc.RadioItems(
+                    id='period-selector',
+                    options = [
+                        {'label': '1 mth', 'value': '1mo'},
+                        {'label': '3 mth', 'value': '3mo'},
+                        {'label': '6 mth', 'value': '6mo'},
+                        {'label': '1 yr', 'value': '1y'},
+                        {'label': '5 yr', 'value': '5y'}
+                    ],
+                    value = "6mo",
+
+                    #labelStyle is done in CSS 
+                    labelStyle={
+                        'textAlign': 'center',
+                        'display': 'inline-block', 
+                        'color': 'white',
+                        'font-family': 'Arial',
+                        'font-size' : '15px',
+                        'padding-right': '30px'
+                    },
+
+                    style={
+                        'textAlign': 'center'
+                    }
+                )
+                ])
+
+app.layout = html.Div([
+    dcc.Location(id="url", refresh=False),
+    SIDEBAR,
+    CONTENT,
+    CALLBACK
+])
+
+
+
+""" Plotly graph """
+@app.callback(
+    Output("page-content", "children"),
+    Output("period-selector", "style"),
+    Input("url", "pathname"),
+    Input('period-selector', 'value')
+)
+
+def render_page_content(pathname, period_selector):
+    """ App routing from sidebar to content depending on path """
+
+    hidden_style = {'display': 'none'}
+    #  It would be best to make visible_style a module level variable.
+    #  Use it for both the app layout and this callback function.
+    #  That way if you want to change its location or look you only have to edit one location.
+    visible_style = {'textAlign': 'center'} 
+
+    if pathname == '/':
+        return page_1.layout, hidden_style
     
-    # Format performance label based on +ve or -ve 
-    performance_calc = ((data[-1] / data[0]) - 1) * 100
-    if performance_calc >= 0:
-        performance = '+' + str(round(performance_calc,2))
-    else:
-        performance = str(round(performance_calc,2))
+    if pathname == '/test':
+        return 'Test', hidden_style
+    
+    else:  # If pathname isn't defined above, assume is stock ticker and calls graph below 
+        interval = '1d'
 
-    return performance
+        quote = yf.Ticker(pathname)
+        hist = quote.history(period_selector, interval)
+        df = hist.round(decimals=2)
+        
+        last_price = df.iloc[-1, 0] # return first row (-1)
 
+        #Performance over period calc
+        performance_calc = ((df.iloc[-1, 0] / df.iloc[0, 0]) - 1) * 100
+        if performance_calc >= 0:
+            performance = '+' + str(round(performance_calc,2))
+        else:
+            performance = str(round(performance_calc,2))
+        
+        #Define color of performance % 
+        performance_int = (round(performance_calc,2))
+        
+        if performance_int >= 0:
+            perf_color = 'lime'
+        else:
+            perf_color = 'red'
+        
+        fig = px.line(df, 
+            x=df.index, y=df["Close"], 
+            title='NVDA',
+            template="plotly_dark",
+            color_discrete_sequence=['lime'],
+            labels = {'Date': ''}
+            )
+        
+        if interval == '1mo':
+            d_tick='604800000'  #7 days in milliseconds. Datetime format requires ms input. 
+        elif interval == '5y':
+            d_tick = 'M12'
+        else:
+            d_tick='M1'
 
-# Instantiate Class by assigning Flask to a variable 
-app = Flask(__name__) #Flask is a Class
+        fig.update_xaxes(
+            dtick=d_tick,
+            showgrid=False,            
+        )
 
+        fig.update_yaxes(
+            showgrid=False,
+            visible=False
+        )
+        #Last price annotation 
+        fig.add_annotation(dict(font=dict(color='white',size=40, family='Arial Black'),
+            x=0,
+            y=1.2,
+            showarrow=False,
+            text=str(last_price),
+            textangle=0,
+            xanchor='left',
+            xref="paper",
+            yref="paper")
+        )
 
-#Define the root route
-@app.route('/index') #Which URL should trigger the function? app is an instance of Flask class which has the method .route 
-def index():
-    """ Serve webpage """
-    return render_template("index.html")
+        #Performance annotation 
+        fig.add_annotation(dict(font=dict(color=perf_color,size=25, family='Arial'),
+            x=0.14,
+            y=1.12,
+            showarrow=False,
+            text=str(performance) + '%',
+            textangle=0,
+            xanchor='left',
+            xref="paper",
+            yref="paper")
+        )
 
+        fig.update_traces(hovertemplate=None)
+        
+        fig.update_layout(
+            plot_bgcolor='#060606',
+            paper_bgcolor='#060606',
+            font_color='white',
+            title=dict(   
+                y = 0.82,  #Adjust location of stock ticker title 
+                x = 0.92,
+                xanchor = 'center',
+                yanchor = 'top',
+                font=dict(
+                    family="Arial",
+                    size=25,
+                    color='white'
+                    )
+            ),
+            hovermode = 'x',
+            # xaxis_tickformat = '%b'
+        )
 
-@app.route('/about') # NB: when you use url_for tag it looks for the 'url for' function which = '/about' in this case. 
-                     # Allows you to change the '/about' without changing it in each .html 
-def about():
-    """ Serve webpage """
-    return render_template("about.html")
+        # Return below into children tag above 
+        return [
+                html.H1(pathname,
+                        style={'textAlign':'center'}),
+                dcc.Graph(id='stock-chart',
+                    figure = fig
+                    )
+                ], visible_style
 
-
-
-# Main code starts here
-@app.route('/')
-def home():
-    for k, v in stock_period_dict.items():
-        if k == stock_period_default:
-            stock_period_default_expanded = v
-
-    return render_template("home.html", stock_period_default_expanded=stock_period_default_expanded)
-
-@app.route('/threecols')
-def threecols():
-    NVDA_perf = performance('NVDA', stock_period_default, stock_interval_default)
-
-    return render_template("threecols.html", NVDA_perf=NVDA_perf)
-
-
-@app.route('/index2')
-def index2():
-    return render_template("index2.html")
-
-@app.route('/index3')
-def index3():
-    return render_template("index3.html")
-
-
-# Plot defaults 
-stock_period_default = '6mo'
-stock_interval_default='1d'
-
-stock_period_dict = {'1mo':'1 month', '3mo':'3 months', '6mo':'6 months'}
-
-# Plot routes below 
-
-@app.route('/plot_graph_TWLO')
-def plot_graph_TWLO():
-    return plot_graph('TWLO', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_U')
-def plot_graph_U():
-    return plot_graph('U', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_SQ')
-def plot_graph_SQ():
-    return plot_graph('SQ', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_PYPL')
-def plot_graph_PYPL():
-    return plot_graph('PYPL', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_SDGR')
-def plot_graph_SDGR():
-    return plot_graph('SDGR', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_PLTR')
-def plot_graph_PLTR():
-    return plot_graph('PLTR', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_ETSY')
-def plot_graph_ETSY():
-    return plot_graph('ETSY', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_NVDA')
-def plot_graph_NVDA():
-    return plot_graph('NVDA', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_GOOG')
-def plot_graph_GOOG():
-    return plot_graph('GOOG', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_DIS')
-def plot_graph_DIS():
-    return plot_graph('DIS', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_CRM')
-def plot_graph_CRM():
-    return plot_graph('CRM', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_RTX')
-def plot_graph_RTX():
-    return plot_graph('RTX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_AMZN')
-def plot_graph_AMZN():
-    return plot_graph('AMZN', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_TSM')
-def plot_graph_TSM():
-    return plot_graph('TSM', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_LRCX')
-def plot_graph_LRCX():
-    return plot_graph('LRCX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_MSFT')
-def plot_graph_MSFT():
-    return plot_graph('MSFT', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_SWKS')
-def plot_graph_SWKS():
-    return plot_graph('SWKS', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_ABB')
-def plot_graph_ABB():
-    return plot_graph('ABB.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_VGL')
-def plot_graph_VGL():
-    return plot_graph('VGL.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_EXP')
-def plot_graph_EXP():
-    return plot_graph('EXP.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_EML')
-def plot_graph_EML():
-    return plot_graph('EML.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_SHL')
-def plot_graph_SHL():
-    return plot_graph('SHL.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_CKF')
-def plot_graph_CKF():
-    return plot_graph('CKF.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_ALL')
-def plot_graph_ALL():
-    return plot_graph('ALL.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_REA')
-def plot_graph_REA():
-    return plot_graph('REA.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_HMD')
-def plot_graph_HMD():
-    return plot_graph('HMD.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_AD8')
-def plot_graph_AD8():
-    return plot_graph('AD8.AX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_XRO')
-def plot_graph_XRO():
-    return plot_graph('XRO.AXX', stock_period_default, stock_interval_default)
-
-@app.route('/plot_graph_PDN')
-def plot_graph_PDN():
-    return plot_graph('PDN.AX', stock_period_default, stock_interval_default)
-
-# Small plots
-@app.route('/small_plot_NVDA')
-def small_plot_NVDA():
-    return small_plot('NVDA', stock_period_default, stock_interval_default)
-
-
-# Launch flask
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__=='__main__':
+    app.run_server(debug=True, port=3000)
---
+++ page_1.py
@@ -0,0 +1,8 @@
+from dash import html
+from dash import dcc
+
+layout = html.Div([
+    dcc.Link('Go to Page 1', href='/page-1'),
+    html.Br(),
+    dcc.Link('Go to Page 2', href='/page-2'),
+])
